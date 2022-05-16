0a0f2a2e2a7a59f661b43f699efb085dbeebdb3d
Akshay Narvate <88389553+akshaynarvate@users.noreply.github.com>
2022-05-16 19:00:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import pickle
 import warnings
 
 warnings.filterwarnings("ignore")
-model = pickle.load(open("Model_GYM.pkl","rb")) #loading the created model
+model = pickle.load(open("Model_Sales.pkl","rb")) #loading the created model
 
 
-st.set_page_config(page_title="GYM Application") #tab title
+st.set_page_config(page_title="Future Sales Prediction Application") #tab title
 
 #prediction function
-def predict_status(age, height, weight):
-    input_data = np.asarray([age, height, weight])
+def predict_status(TV, Radio, Newspaper):
+    input_data = np.asarray([TV, Radio, Newspaper])
     input_data = input_data.reshape(1,-1)
     prediction = model.predict(input_data)
     return prediction[0]
@@ -20,35 +20,26 @@ def predict_status(age, height, weight):
 def main():
 
     # titling your page
-    st.title("Fitness Prediction App")
+    st.title("Future Sales Prediction Application")
 
     #getting the input
-    age = st.text_input("Enter your Age")
-    height = st.text_input("Enter your Height in feet")
-    weight = st.text_input("Enter your Weight in kgs")
+    TV = st.text_input("Enter your TV")
+    Radio = st.text_input("Enter your Height in Radio")
+    Newspaper = st.text_input("Enter your Weight in Newspaper")
 
     #predict value
     diagnosis = ""
 
     if st.button("Predict"):
     
-        diagnosis = predict_status(age, height, weight)
-        if diagnosis=="Underweight":
-            st.info("You're Under Weight")
+        diagnosis = predict_status(TV, Radio, Newspaper)
+        st.info("You're Sale is: ", diagnosis)
 
-        elif diagnosis=="Healthy":
-            st.success("You're Healthy")
 
-        elif diagnosis=="Overweight":
-            st.warning("You're Over Weight")
-
-        elif diagnosis=="Obese":
-            st.error("Obesity!")
-
-        else:
-            st.error("Extremely Obese")
-        
-        st.write("Project by Akshay Narvate")
+    else:
+         st.write("Cannot predict please Reenter the values")
+    st.write(" ")    
+    st.write("Project by Akshay Narvate")
     
             
 if __name__=="__main__":
