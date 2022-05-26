894d80f9e33c89cce0cb55fe19143e40d6bac6ca
Akshay Narvate <88389553+akshaynarvate@users.noreply.github.com>
2022-05-26 13:21:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ model = pickle.load(open("Model_Sales.pkl","rb")) #loading the created model
 st.set_page_config(page_title="Future Sales Prediction Application") #tab title
 
 #prediction function
-def predict_status(TV, Radio, Newspaper):
-    input_data = np.asarray([[TV, Radio, Newspaper]])
+def predict_status(TV, Radio):
+    input_data = np.asarray([[TV, Radio]])
     #input_data = input_data.reshape(1,-1)
     prediction = model.predict(input_data)
     return prediction[0]
@@ -25,14 +25,14 @@ def main():
     #getting the input
     TV = st.number_input("Enter your TV Budget(dollars)")
     Radio = st.number_input("Enter your Radio Budget(dollars)")
-    Newspaper = st.number_input("Enter your Newspaper(dollars)")
+    
 
     #predict value
     diagnosis = ""
 
     if st.button("Predict"):
     
-        Sale = predict_status(TV, Radio, Newspaper)
+        Sale = predict_status(TV, Radio)
         st.success("You'r Sale is: ", Sale, " Units")
 
     st.write(" ")    
